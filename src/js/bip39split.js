6f782ea45820147be9795f979fb9612ff8e80381
Pascal Martin <pascal@pascalmartin.io>
2019-08-06 23:37:07 -0400

    Refactoring split and merge to be more easy to make test

---
+++ src/js/bip39split.js
@@ -0,0 +1,103 @@
+
+class Bip39split {
+
+    static SPLIT_ERROR_NAME = "Bip39split split Error";
+    static SPLIT_ERROR_MESSAGE = "Split error detected";
+
+    static MERGE_ERROR_NAME = "Bip39split merge Error";
+    static MERGE_ERROR_MESSAGE = "Merge error detected";
+
+    static split(mnemonicLanguage, mnemonic, nbShares, threshold) {
+        var validationError = {
+            name: this.SPLIT_ERROR_NAME,
+            message: this.SPLIT_ERROR_MESSAGE,
+            isValidThreshold: true,
+            isValidMnemonic: true
+        };
+
+        if (threshold < 2 || threshold > nbShares) {
+            validationError.isValidThreshold = false; // Threshold is invalid. Must be at beetween 2 and number of shares
+        }
+
+        var wordlist = bip39.wordlists[mnemonicLanguage];
+        validationError.isValidMnemonic = bip39.validateMnemonic(mnemonic, wordlist) // Check if Bip39 Mnemonic is valid.		
+
+        if (!validationError.isValidMnemonic || !validationError.isValidThreshold) {
+            throw validationError;
+        }
+
+        var entropy = bip39.mnemonicToEntropy(mnemonic, wordlist);
+        var hash = CryptoJS.SHA3(entropy).toString(); // Version <= 0.0.2 hash is the entropy hash
+        //var hash = CryptoJS.SHA3(mnemonic).toString(); // Version >= 0.0.3 hash is the mnemonic hash
+
+        var shares = secrets.share(entropy, nbShares, threshold, 0);
+
+        return {
+            hash: hash,
+            shares: shares
+        };
+    }
+
+    static merge(mnemonicLanguage, shares, hash) {
+        var wordlist = bip39.wordlists[mnemonicLanguage];
+
+        try {
+            var entropy = secrets.combine(shares)
+            var mnemonic = bip39.entropyToMnemonic(entropy, wordlist);
+            var isValidMnemonic = bip39.validateMnemonic(mnemonic, wordlist);
+        }
+        catch (err) {
+            isValidMnemonic = false
+        }
+
+        if (!isValidMnemonic) {
+            throw {
+                name: this.MERGE_ERROR_NAME,
+                message: this.MERGE_ERROR_MESSAGE,
+                isValidShares: false,
+            };
+        }
+
+        var restoreEntropyHash = CryptoJS.SHA3(entropy).toString(); // Version <= 0.0.2 hash is the entropy hash
+        var restoreMnemonicHash = CryptoJS.SHA3(mnemonic).toString(); // Version >= 0.0.3 hash is the mnemonic hash
+        var isValidHash = false;
+        if (hash) {
+            if (restoreEntropyHash == hash) {
+                isValidHash = true;
+            } else if (restoreMnemonicHash == hash) {
+                isValidHash = true;
+            } else {
+                // TRY Auto Correct mnemonic language
+                try {
+                    var languages = ["english", "french", "italian", "spanish", "korean", "japanese", "chinese_traditional", "chinese_simplified"];
+                    for (let i = 0; i < languages.length; i++) {
+                        var language = languages[i];
+                        var wordlist = bip39.wordlists[language];
+                        var entropy = secrets.combine(shares)
+                        var autoMnemonic = bip39.entropyToMnemonic(entropy, wordlist);
+                        var isValidMnemonic = bip39.validateMnemonic(autoMnemonic, wordlist);
+                        var autoRestoreMnemonicHash = CryptoJS.SHA3(autoMnemonic).toString(); // Version >= 0.0.3 hash is the mnemonic hash
+                        if (isValidMnemonic && autoRestoreMnemonicHash == hash) {
+                            return {
+                                mnemonic: autoMnemonic,
+                                mnemonicLanguage: language,
+                                mnemonicHash: autoRestoreMnemonicHash,
+                                isValidHash: true
+                            }
+                        }
+                    }
+                }
+                catch (err) {
+                    // ignore
+                }
+            }
+        }
+
+        return {
+            mnemonic: mnemonic,
+            mnemonicLanguage: mnemonicLanguage,
+            mnemonicHash: restoreMnemonicHash,
+            isValidHash: isValidHash
+        }
+    }
+}
